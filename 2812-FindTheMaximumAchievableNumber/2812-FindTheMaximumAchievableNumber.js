2af674d62f32730180b2b284488c25d1796c8ddb
mann patel <mann.patel.cg@gmail.com>
2026-01-01 04:56:38 +0530

    Create 2812-FindTheMaximumAchievableNumber

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.js
@@ -0,0 +1,9 @@
+// Last updated: 3/6/2026, 2:08:30 PM
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num+2*t
+};
