6b3600493a381301dd5aaaa9928c2e2144391ad8
mann patel <mann.patel.cg@gmail.com>
2026-01-01 00:25:08 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.js
@@ -0,0 +1,9 @@
+// Last updated: 3/6/2026, 2:09:43 PM
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
