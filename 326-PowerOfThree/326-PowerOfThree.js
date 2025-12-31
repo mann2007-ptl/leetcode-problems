2a6f8b80dcb521569d8fb43d3d574b477a010d20
mann patel <mann.patel.cg@gmail.com>
2026-01-01 05:15:28 +0530

    Create 326-PowerOfThree

---
+++ 326-PowerOfThree/326-PowerOfThree.js
@@ -0,0 +1,19 @@
+// Last updated: 3/6/2026, 2:09:21 PM
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+     if(n<=0){
+        return false;
+    }
+    while(n%3==0){
+        n=n/3;
+    }
+    if(n===1){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
