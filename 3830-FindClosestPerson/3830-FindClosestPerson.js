1e39a43cccc59b419e58bbd103990b24c8d4708e
mann patel <mann.patel.cg@gmail.com>
2026-01-01 00:40:48 +0530

    Create 3830-FindClosestPerson

---
+++ 3830-FindClosestPerson/3830-FindClosestPerson.js
@@ -0,0 +1,21 @@
+// Last updated: 3/6/2026, 2:08:10 PM
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    let a = Math.abs(z-x);
+    let b = Math.abs(z-y);
+
+    if(a<b){
+        return 1;
+    }
+    else if(a>b){
+        return 2;
+    }
+    else{
+        return 0;
+    }
+};
