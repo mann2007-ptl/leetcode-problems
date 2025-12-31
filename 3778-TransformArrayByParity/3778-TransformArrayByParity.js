1f156027fe0d90b64a24ac5f4a9db782d4490bdb
mann patel <mann.patel.cg@gmail.com>
2026-01-01 05:11:49 +0530

    Create 3778-TransformArrayByParity

---
+++ 3778-TransformArrayByParity/3778-TransformArrayByParity.js
@@ -0,0 +1,16 @@
+// Last updated: 3/6/2026, 2:08:11 PM
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var transformArray = function(nums) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]%2==0){
+            nums[i]=0;
+        }
+        else{
+            nums[i]=1;
+        }
+    }
+    return nums.sort((a,b)=> a-b)
+};
