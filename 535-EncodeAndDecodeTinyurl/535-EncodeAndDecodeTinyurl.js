fa88697ad94fa0b828f90a84728a39b1494b9a4b
mann patel <mann.patel.cg@gmail.com>
2026-01-01 05:25:14 +0530

    Create 535-EncodeAndDecodeTinyurl

---
+++ 535-EncodeAndDecodeTinyurl/535-EncodeAndDecodeTinyurl.js
@@ -0,0 +1,25 @@
+// Last updated: 3/6/2026, 2:09:14 PM
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+    return longUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    return shortUrl
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
