534d4572ff15ccce1c9be591a6bc41afc42ce5dd
MANN PATEL <mann.patel.cg@gmail.com>
2026-03-11 12:02:44 +0530

    File updated at 3/11/2026, 12:01:49 PM

--- 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.js
+++ 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.js
@@ -1,22 +1,20 @@
-// Last updated: 3/11/2026, 12:00:57 PM
-1/**
-2 * @param {number[]} nums
-3 * @return {number}
-4 */
-5var findNumbers = function(nums) {
-6    let value = 0;
-7    for(let i=0;i<nums.length;i++){
-8        let count=0;
-9        let temp = nums[i];
-10        while(temp!=0){
-11            let digit = temp%10;
-12            count++;
-13            temp=Math.floor(temp/10);
-14        }
-15
-16        if(count%2==0){
-17            value++;
-18        }
-19    }
-20    return value;
-21};
+// Last updated: 3/11/2026, 12:01:49 PM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let res = 0;
+    for(let i=0; i<nums.length; i++){
+      let count = 0;
+       let num = nums[i];
+       while(num>0){
+        count++;
+        num = Math.floor(num/10);
+       }
+       if(count%2===0){
+        res++;
+       }
+    }
+    return res;
+};
