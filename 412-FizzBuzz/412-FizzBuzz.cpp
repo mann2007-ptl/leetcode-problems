0b591114da20760edb68c659d76dfcbc9892f3c5
MANN PATEL <mann.patel.cg@gmail.com>
2026-03-27 17:10:31 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.cpp
@@ -0,0 +1,23 @@
+// Last updated: 3/27/2026, 5:09:30 PM
+1class Solution {
+2public:
+3    vector<string> fizzBuzz(int n) {
+4        vector<string> answer;
+5
+6        for(int i=1;i<=n;i++){
+7            if(i%3==0 && i%5==0){
+8                answer.push_back("FizzBuzz");
+9            }
+10            else if(i%3==0){
+11                answer.push_back("Fizz");
+12            }
+13            else if(i%5==0){
+14                answer.push_back("Buzz");
+15            }
+16            else{
+17                answer.push_back(to_string(i));
+18            }
+19        }
+20        return answer;
+21    }
+22};
