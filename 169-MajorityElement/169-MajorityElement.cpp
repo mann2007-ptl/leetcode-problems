4ae352913a43a9aa32b47521397dfba4370ac31d
MANN PATEL <mann.patel.cg@gmail.com>
2026-04-07 09:53:11 +0530

    File updated at 4/7/2026, 9:52:06 AM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,22 +1,22 @@
-// Last updated: 4/7/2026, 9:33:04 AM
+// Last updated: 4/7/2026, 9:52:06 AM
 1class Solution {
 2public:
 3    int majorityElement(vector<int>& nums) {
-4        int n = nums.size();
-5
-6        for(int i=0;i<n;i++){
-7            int count = 0;
-8            for(int j=0;j<n;j++){
-9                if(nums[i]==nums[j]){
-10                    count++;
-11                }
-12            }
-13
-14            if(count>n/2){
-15                return nums[i];
-16            }
-17        }
-18
-19        return -1;
+4        int number = 0;
+5        int count = 0;
+6
+7        for(int i=0;i<nums.size();i++){
+8            if(count==0){
+9                number = nums[i];
+10                count = 1;
+11            }
+12            else if(nums[i]==number){
+13                count++;
+14            }
+15            else{
+16                count--;
+17            }
+18        }
+19        return number;
 20    }
 21};
