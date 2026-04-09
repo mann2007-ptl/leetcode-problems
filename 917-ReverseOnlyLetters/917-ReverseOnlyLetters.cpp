9dad7cd8b880e72f2827656e25360acf0bbb7b33
MANN PATEL <mann.patel.cg@gmail.com>
2026-04-09 10:18:37 +0530

    File updated at 4/9/2026, 10:17:32 AM

--- 917-ReverseOnlyLetters/917-ReverseOnlyLetters.cpp
+++ 917-ReverseOnlyLetters/917-ReverseOnlyLetters.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/9/2026, 10:03:27 AM
+// Last updated: 4/9/2026, 10:17:32 AM
 1class Solution {
 2public:
 3    string reverseOnlyLetters(string s) {
@@ -6,18 +6,16 @@
 5        int j = s.size() - 1;
 6
 7        while (i < j) {
-8        if(!(isalpha(s[i]))){
+8            if (!(isalpha(s[i]))) {
 9                i++;
-10        }
-11        else if(!(isalpha(s[j]))){
-12                j--;
-13        }
-14        else{
-15                swap(s[i], s[j]);
-16                i++;
-17                j--;
-18        }
-19        }
-20        return s;
-21    }
-22};
+10            } else if (!(isalpha(s[j]))) {
+11                j--;
+12            } else {
+13                swap(s[i], s[j]);
+14                i++;
+15                j--;
+16            }
+17        }
+18        return s;
+19    }
+20};
