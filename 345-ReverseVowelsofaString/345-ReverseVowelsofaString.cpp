b20d8fcf030ad1e78d82f74bec52f789d5d97c0d
MANN PATEL <mann.patel.cg@gmail.com>
2026-04-09 10:18:45 +0530

    File updated at 4/9/2026, 10:17:40 AM

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.cpp
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.cpp
@@ -1,23 +1,23 @@
-// Last updated: 4/9/2026, 9:43:23 AM
-1class Solution {
-2public:
-3    string reverseVowels(string s) {
-4        int i=0;
-5        int j=s.size()-1;
-6
-7        while(i<j){
-8            if(!(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o'||s[i]=='u'||s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O'||s[i]=='U')){
-9                i++;
-10            }
-11            else if(!(s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o'||s[j]=='u'||s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O'||s[j]=='U')){
-12                j--;
-13            }
-14            else{
-15                swap(s[i],s[j]);
-16                i++;
-17                j--;
-18            }
-19        }
-20        return s;
-21    }
-22};
+// Last updated: 4/9/2026, 10:17:40 AM
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+    string reverseVowels(string s) {
+        int i = 0, n = s.size(), j = n - 1;
+        while (i < j) {
+            while (i<j && !isVowel(s[i])) {
+                i++;
+            }
+            while (i<j && !isVowel(s[j])) {
+                j--;
+            }
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
