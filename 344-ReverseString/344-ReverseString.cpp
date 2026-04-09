66042e7910f459dc5945b689fb89896a1edc81c7
MANN PATEL <mann.patel.cg@gmail.com>
2026-04-09 10:18:51 +0530

    File updated at 4/9/2026, 10:17:45 AM

--- 344-ReverseString/344-ReverseString.cpp
+++ 344-ReverseString/344-ReverseString.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/9/2026, 9:31:12 AM
+// Last updated: 4/9/2026, 10:17:45 AM
 1class Solution {
 2public:
 3    void reverseString(vector<char>& s) {
@@ -6,9 +6,11 @@
 5        int j=s.size()-1;
 6
 7        while(i<j){
-8          swap(s[i],s[j]);
-9            i++;
-10            j--;
-11        }
-12    }
-13};
+8            int temp = s[i];
+9            s[i]=s[j];
+10            s[j]=temp;
+11            i++;
+12            j--;
+13        }
+14    }
+15};
