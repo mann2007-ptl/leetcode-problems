657f109a166d8826ddae28d11b5c7019964f5025
MANN PATEL <mann.patel.cg@gmail.com>
2026-04-14 15:53:45 +0530

    Create 58-LengthofLastWord

---
+++ 58-LengthofLastWord/58-LengthofLastWord.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/14/2026, 3:52:37 PM
+1class Solution {
+2public:
+3    int lengthOfLastWord(string s) {
+4        int len = 0;
+5
+6        for(int i=s.length()-1;i>=0;i--){
+7            if(s[i]!= ' '){
+8                len++;
+9            }
+10            else if(len > 0){
+11                break;
+12            }
+13        }
+14        return len;
+15    }
+16};
