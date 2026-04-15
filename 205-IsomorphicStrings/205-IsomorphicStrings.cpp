58318bb407faf642981a5030ad53c9f2d9a18d01
MANN PATEL <mann.patel.cg@gmail.com>
2026-04-15 11:58:25 +0530

    File updated at 4/15/2026, 11:57:16 AM

--- 205-IsomorphicStrings/205-IsomorphicStrings.cpp
+++ 205-IsomorphicStrings/205-IsomorphicStrings.cpp
@@ -1,26 +1,21 @@
-// Last updated: 4/15/2026, 11:55:16 AM
-1class Solution {
-2public:
-3    bool isIsomorphic(string s, string t) {
-4        unordered_map<char,char> s1;
-5        unordered_map<char,char> t1;
-6
-7        for(int i=0;i<s.size();i++){
-8            char ch1 = s[i];
-9            char ch2 = t[i];
-10
-11            if(s1.count(ch1) && s1[ch1]!=ch2){
-12                return false;
-13            }
-14
-15            if(t1.count(ch2) && t1[ch2]!=ch1){
-16                return false;
-17            }
-18
-19            s1[ch1]=ch2;
-20            t1[ch2]=ch1;
-21        }
-22
-23        return true;
-24    }
-25};
+// Last updated: 4/15/2026, 11:57:16 AM
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> m1(128, -1); 
+        vector<int> m2(128, -1); 
+
+        for (int i = 0; i < s.size(); i++) {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if (m1[c1] != -1 && m1[c1] != c2) return false;
+            if (m2[c2] != -1 && m2[c2] != c1) return false;
+
+            m1[c1] = c2;
+            m2[c2] = c1;
+        }
+
+        return true;
+    }
+};
