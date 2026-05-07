5d0d350f2a4d553591847740223ef72b8da0e2c8
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-07 09:14:29 +0530

    File updated at 5/7/2026, 9:13:10 AM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.cpp
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.cpp
@@ -1,8 +1,7 @@
-// Last updated: 5/7/2026, 9:12:26 AM
+// Last updated: 5/7/2026, 9:13:10 AM
 1class Solution {
 2public:
 3    int sum(int num1, int num2) {
-4        int result = num1 + num2;
-5        return result;
-6    }
-7};
+4        return num1 + num2;
+5    }
+6};
