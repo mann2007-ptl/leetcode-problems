37231fc72a742f372bc878317c09781f315c91d7
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-07 08:42:17 +0530

    File updated at 5/7/2026, 8:40:58 AM

--- 1342-NumberofStepstoReduceaNumbertoZero/1342-NumberofStepstoReduceaNumbertoZero.cpp
+++ 1342-NumberofStepstoReduceaNumbertoZero/1342-NumberofStepstoReduceaNumbertoZero.cpp
@@ -1,19 +1,20 @@
-// Last updated: 5/7/2026, 8:25:54 AM
+// Last updated: 5/7/2026, 8:40:58 AM
 1class Solution {
 2public:
 3    int numberOfSteps(int num) {
-4        int step = 0;
+4      int steps = 0;
 5
-6        while(num > 0){
-7            if(num%2==0){
-8                num=num/2;
-9                step++;
-10            }
-11            else{
-12                num=num-1;
-13                step++;
-14            }
-15        }
-16        return step;
-17    }
-18};
+6      while(num > 0){
+7        if(num % 2 == 0){
+8            num=num/2;
+9            steps++;
+10        }
+11        else{
+12           num = num - 1;
+13           steps++; 
+14        }
+15      }
+16
+17      return steps;
+18    }
+19};
