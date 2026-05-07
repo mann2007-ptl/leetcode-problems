ac85ea2fee8b6a6b95d1eea2715a424a9e115fd2
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-07 09:13:44 +0530

    File updated at 5/7/2026, 9:12:26 AM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.cpp
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.cpp
@@ -1,25 +1,8 @@
-// Last updated: 5/7/2026, 9:06:08 AM
+// Last updated: 5/7/2026, 9:12:26 AM
 1class Solution {
 2public:
-3    bool judgeCircle(string moves) {
-4     int x = 0;
-5     int y = 0;
-6
-7     for(char move : moves){
-8        if(move == 'U'){
-9            y++;
-10        }
-11        else if(move == 'D'){
-12            y--;
-13        }
-14        else if(move == 'R'){
-15            x++;
-16        }
-17        else{
-18            x--;
-19        }
-20     }
-21
-22     return (x==0 && y==0);
-23    }
-24};
+3    int sum(int num1, int num2) {
+4        int result = num1 + num2;
+5        return result;
+6    }
+7};
