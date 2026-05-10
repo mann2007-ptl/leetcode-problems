41961d645cc1e26e5fb1a40cad6a98a16a245ea9
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-10 09:16:20 +0530

    File updated at 5/10/2026, 9:15:01 AM

--- 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.cpp
+++ 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.cpp
@@ -1,22 +1,23 @@
-// Last updated: 3/17/2026, 11:01:13 AM
+// Last updated: 5/10/2026, 9:15:01 AM
 1class Solution {
 2public:
 3    int mostWordsFound(vector<string>& sentences) {
-4        int maxWords = 0;
+4        int maxWord = 0;
 5
-6        for(int i=0;i<sentences.size();i++){
+6        for(int i = 0; i < sentences.size(); i++){
 7            int count = 1;
 8
-9            for(int j=0;j<sentences[i].size();j++){
+9            for(int j = 0; j < sentences[i].size(); j++){
 10                if(sentences[i][j] == ' '){
 11                    count++;
 12                }
 13            }
-14            if(count>maxWords){
-15                maxWords = count;
-16            }
-17        }
-18
-19        return maxWords;
-20    }
-21};
+14
+15            if(count > maxWord){
+16                maxWord = count;
+17            }
+18        }
+19
+20        return maxWord;
+21    }
+22};
