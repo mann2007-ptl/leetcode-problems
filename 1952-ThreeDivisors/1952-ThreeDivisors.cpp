6d5b3cbeaf033a7124ccb5a8518477774c0e3f05
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-10 09:00:47 +0530

    File updated at 5/10/2026, 8:59:28 AM

--- 1952-ThreeDivisors/1952-ThreeDivisors.cpp
+++ 1952-ThreeDivisors/1952-ThreeDivisors.cpp
@@ -1,16 +1,18 @@
-// Last updated: 3/30/2026, 2:14:24 PM
+// Last updated: 5/10/2026, 8:59:28 AM
 1class Solution {
 2public:
 3    bool isThree(int n) {
 4        int count = 0;
-5        for(int i=1;i<=n;i++){
-6            if(n%i==0){
-7                count++;
-8            }
-9        }
-10        if(count==3){
-11            return true;
-12        }
-13        return false;
-14    }
-15};
+5
+6        for(int i=1;i<=n;i++){
+7            if(n%i==0){
+8                count++;
+9            }
+10        }
+11
+12        if(count==3){
+13            return true;
+14        }
+15        return false;
+16    }
+17};
