e17ad6ac62005a93d71c96218ba0b99cd1945fb6
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-11 09:43:00 +0530

    Create

---
+++ problem-1778472699975/problem-1778472699975.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/11/2026, 9:41:39 AM
+1class Solution {
+2public:
+3    int lengthOfLastWord(string s) {
+4       int len = 0;
+5
+6       for(int i=s.length()-1;i>=0;i--){
+7        if(s[i]!=' '){
+8            len++;
+9        }
+10        else if(len>0){
+11            break;
+12        }
+13       }
+14 return len;
+15
+16    }
+17};
