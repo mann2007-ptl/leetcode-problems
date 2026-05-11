49e4dc3b01e053a5c9f3c60c483839d430b0bb1a
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-11 09:28:17 +0530

    File updated at 5/11/2026, 9:26:56 AM

--- 125-ValidPalindrome/125-ValidPalindrome.cpp
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -1,32 +1,31 @@
-// Last updated: 4/24/2026, 9:46:19 AM
+// Last updated: 5/11/2026, 9:26:56 AM
 1class Solution {
 2public:
 3    bool isPalindrome(string s) {
 4        string ans;
 5
 6        for (char st : s) {
-7            if ((st >= 'A' && st <= 'Z') ||(st >= 'a' && st <= 'z') ||
-8            (st >= '0' && st <= '9')) {
-9
-10                if (st >= 'A' && st <= 'Z') {
-11                    st = st + 32;
-12                }
-13
-14                ans += st;
-15            }
-16        }
-17
-18        int i = 0;
-19        int j = ans.size() - 1;
-20
-21        while (i < j) {
-22            if (ans[i] != ans[j]) {
-23                return false;
-24            }
-25            i++;
-26            j--;
-27        }
-28
-29        return true;
-30    }
-31};
+7            if ((st >= 'A' && st <= 'Z') || (st >= 'a' && st <= 'z') ||
+8                (st >= '0' && st <= '9')) {
+9                    if (st >= 'A' && st <= 'Z') {
+10                        st = st + 32;
+11                    }
+12                    ans += st;
+13                }
+14        }
+15
+16        int i=0;
+17        int j=ans.size()-1;
+18
+19        while(i<j){
+20            if(ans[i]!=ans[j]){
+21                return false;
+22            }
+23
+24            i++;
+25            j--;
+26        }
+27
+28        return true;
+29    }
+30};
