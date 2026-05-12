70045d4424f33838515ba57e6de127376fffc7b3
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-12 09:47:07 +0530

    Create 2574-LeftandRightSumDifferences

---
+++ 2574-LeftandRightSumDifferences/2574-LeftandRightSumDifferences.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/12/2026, 9:45:47 AM
+1class Solution {
+2public:
+3    vector<int> leftRightDifference(vector<int>& nums) {
+4        vector<int> ans;
+5
+6        for(int i=0;i<nums.size();i++){
+7            int left=0;
+8            int right=0;
+9
+10            for(int j=0;j<i;j++){
+11                left=left+nums[j];
+12            }
+13            for(int j=i+1;j<nums.size();j++){
+14                right=right+nums[j];
+15            }
+16            ans.push_back(abs(left-right));
+17        }
+18
+19        return ans;
+20    }
+21};
