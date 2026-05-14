ef7c2b44d7091ef0200b515db725f110fcf7ec9d
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-14 09:48:05 +0530

    File updated at 5/14/2026, 9:46:44 AM

--- 771-JewelsandStones/771-JewelsandStones.cpp
+++ 771-JewelsandStones/771-JewelsandStones.cpp
@@ -1,25 +1,17 @@
-// Last updated: 5/14/2026, 9:41:05 AM
+// Last updated: 5/14/2026, 9:46:44 AM
 1class Solution {
 2public:
 3    int numJewelsInStones(string jewels, string stones) {
-4        
-5        int count = 0;  // stores how many jewels we found
-6        
-7        // Loop through each stone
-8        for (char s : stones) {
-9            
-10            // For each stone, check all jewels
-11            for (char j : jewels) {
-12                
-13                // If stone matches a jewel
-14                if (s == j) {
-15                    count++;   // increase count
-16                    
-17                    break;     // stop checking more jewels for this stone
-18                }
-19            }
-20        }
-21        
-22        return count;  // return final answer
-23    }
-24};
+4        int count = 0;
+5
+6        for(char s : stones){
+7            for(char j : jewels){
+8                if(s==j){
+9                    count++;
+10                    break;
+11                }
+12            }
+13        }
+14        return count;
+15    }
+16};
