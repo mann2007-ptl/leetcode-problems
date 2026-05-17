060c97d9647f739a957b38be709f1cfb096b0dda
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-17 09:00:22 +0530

    File updated at 5/17/2026, 8:58:59 AM

--- 2124-CheckifAllA'sAppearsBeforeAllB's/2124-CheckifAllA'sAppearsBeforeAllB's.cpp
+++ 2124-CheckifAllA'sAppearsBeforeAllB's/2124-CheckifAllA'sAppearsBeforeAllB's.cpp
@@ -1,17 +1,19 @@
-// Last updated: 5/17/2026, 8:44:29 AM
+// Last updated: 5/17/2026, 8:58:59 AM
 1class Solution {
 2public:
-3    bool checkString(string s) {
-4        bool seenB = false;
-5
-6        for(char c : s){
-7            if(c == 'b'){
-8                seenB = true;
-9            }
-10            if(c == 'a' && seenB){
-11                return false;
-12            }
-13        } 
-14        return true;
-15    }
-16};
+3    bool isHappy(int n) {
+4        
+5        while(n!=1 && n !=4){
+6           int sum = 0;
+7
+8           while(n > 0){
+9            int digit = n % 10;
+10            sum = sum + digit*digit;
+11            n=n/10;
+12           } 
+13
+14           n = sum;
+15        }
+16       return n==1;
+17    }
+18};
