a5c60842da646f6ae91e26d50d1751250a8b7e8f
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-21 09:41:11 +0530

    File updated at 5/21/2026, 9:39:47 AM

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.cpp
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.cpp
@@ -1,7 +1,35 @@
-// Last updated: 5/21/2026, 9:35:02 AM
+// Last updated: 5/21/2026, 9:39:47 AM
 1class Solution {
 2public:
-3    bool isStrictlyPalindromic(int n) {
-4       return false; 
-5    }
-6};
+3    
+4    bool sumOfNumberAndReverse(int num) {
+5
+6        // Try all numbers from 0 to num
+7        for(int i = 0; i <= num; i++) {
+8
+9            // Copy i into n
+10            // because we will change n while reversing
+11            int n = i;
+12
+13            // Variable to store reversed number
+14            int rev = 0;
+15
+16            // Reverse the number
+17            while(n) {
+18
+19                // Take last digit and add to rev
+20                rev = rev * 10 + n % 10;
+21
+22                // Remove last digit from n
+23                n /= 10;
+24            }
+25
+26            // Check if original number + reverse equals num
+27            if(i + rev == num)
+28                return true;
+29        }
+30
+31        // No valid number found
+32        return false;
+33    }
+34};
