e61e007adc3ac3f2d12109dcb86761bfee20f575
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-25 09:44:01 +0530

    File updated at 5/25/2026, 9:42:35 AM

--- 2442-CountNumberofDistinctIntegersAfterReverseOperations/2442-CountNumberofDistinctIntegersAfterReverseOperations.cpp
+++ 2442-CountNumberofDistinctIntegersAfterReverseOperations/2442-CountNumberofDistinctIntegersAfterReverseOperations.cpp
@@ -1,29 +1,21 @@
-// Last updated: 5/25/2026, 9:32:42 AM
+// Last updated: 5/25/2026, 9:42:35 AM
 1class Solution {
 2public:
 3    int countDistinctIntegers(vector<int>& nums) {
-4        // set stores only unique values
-5        set<int> s;
-6
-7        // Traverse every number in array
-8        for (int x : nums) {
-9            // Insert original number
-10            s.insert(x);
+4       set<int> s;
+5
+6       for(int x : nums){
+7        s.insert(x);
+8
+9        int rev = 0;
+10        int n = x;
 11
-12            int rev = 0;
-13            int n = x;
-14
-15            // Reverse the number
-16            while (n > 0) {
-17                // Take last digit and add to rev
-18                rev = rev * 10 + (n % 10);
-19                // Remove last digit
-20                n /= 10;
-21            }
-22            // Insert reversed number
-23            s.insert(rev);
-24        }
-25        // Return count of unique numbers
-26        return s.size();
-27    }
-28};
+12        while(n>0){
+13            rev = rev * 10 + (n%10);
+14            n=n/10;
+15        }
+16        s.insert(rev);
+17       }
+18       return s.size();
+19    }
+20};
