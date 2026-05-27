d110ee8dfd1dc614f25d99530bb195375ed4a196
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-27 09:46:03 +0530

    File updated at 5/27/2026, 9:44:36 AM

--- 2315-CountAsterisks/2315-CountAsterisks.cpp
+++ 2315-CountAsterisks/2315-CountAsterisks.cpp
@@ -1,16 +1,20 @@
-// Last updated: 5/27/2026, 9:40:33 AM
+// Last updated: 5/27/2026, 9:44:36 AM
 1class Solution {
 2public:
-3    char repeatedCharacter(string s) {
-4      set<char> st;
+3    int countGoodSubstrings(string s) {
+4        int ans = 0;
 5
-6      for(char c : s){
-7        if(st.count(c)){
-8            return c;
-9        }
-10        st.insert(c);
-11      }
-12
-13      return ' ';
-14    }
-15};
+6        for(int i = 0; i + 2 < s.length(); i++) {
+7
+8            char a = s[i];
+9            char b = s[i + 1];
+10            char c = s[i + 2];
+11
+12            if(a != b && b != c && a != c) {
+13                ans++;
+14            }
+15        }
+16
+17        return ans;
+18    }
+19};
