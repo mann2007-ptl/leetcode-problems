23005f956d4c4e9615f343e872b2a49e8a2faca9
MANN PATEL <mann.patel.cg@gmail.com>
2026-05-27 09:48:05 +0530

    File updated at 5/27/2026, 9:46:38 AM

--- 2315-CountAsterisks/2315-CountAsterisks.cpp
+++ 2315-CountAsterisks/2315-CountAsterisks.cpp
@@ -1,20 +1,24 @@
-// Last updated: 5/27/2026, 9:44:36 AM
-1class Solution {
-2public:
-3    int countGoodSubstrings(string s) {
-4        int ans = 0;
-5
-6        for(int i = 0; i + 2 < s.length(); i++) {
-7
-8            char a = s[i];
-9            char b = s[i + 1];
-10            char c = s[i + 2];
-11
-12            if(a != b && b != c && a != c) {
-13                ans++;
-14            }
-15        }
-16
-17        return ans;
-18    }
-19};
+// Last updated: 5/27/2026, 9:46:38 AM
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+
+        int count = 0;
+
+        if(s.size() < 3){
+            return 0;
+        }
+
+        for(int i = 0; i <= s.size() - 3; i++){
+
+            if(s[i] != s[i+1] &&
+               s[i] != s[i+2] &&
+               s[i+1] != s[i+2]){
+
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
