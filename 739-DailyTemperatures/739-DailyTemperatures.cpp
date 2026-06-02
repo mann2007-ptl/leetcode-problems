e97c890bd507a8b4078649c1304578f7e5bd2396
MANN PATEL <mann.patel.cg@gmail.com>
2026-06-02 12:01:38 +0530

    File updated at 6/2/2026, 12:01:38 PM

--- 739-DailyTemperatures/739-DailyTemperatures.cpp
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -1,19 +1,28 @@
-// Last updated: 6/2/2026, 11:21:43 AM
+// Last updated: 6/2/2026, 12:01:38 PM
 1class Solution {
 2public:
-3    vector<int> dailyTemperatures(vector<int>& temperatures) {
-4        int n = temperatures.size();
-5        vector<int> ans(n, 0);
-6        stack<int> st;
+3    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+4        vector<int> ans;
+5        int found = 0;
+6        int nextGreater = -1;
 7
-8        for (int i = 0; i < n; i++) {
-9            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
-10                int prev = st.top();
-11                st.pop();
-12                ans[prev] = i - prev;
-13            }
-14            st.push(i);
-15        }
-16        return ans;
-17    }
-18};
+8        for (int i = 0; i < nums1.size(); i++) {
+9            found = 0;
+10            nextGreater = -1;
+11
+12            for (int j = 0; j < nums2.size(); j++) {
+13                if (nums2[j] == nums1[i]) {
+14                    found = 1;
+15                }
+16                else if (found && nums2[j] > nums1[i]) {
+17                    nextGreater = nums2[j];
+18                    break;
+19                }
+20            }
+21
+22            ans.push_back(nextGreater);
+23        }
+24
+25        return ans;
+26    }
+27};
