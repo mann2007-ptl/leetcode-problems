bb9425c9ecc761d69d20f63fd89f41edd3eea978
MANN PATEL <mann.patel.cg@gmail.com>
2026-06-03 22:04:24 +0530

    Create 260-SingleNumberIII

---
+++ 260-SingleNumberIII/260-SingleNumberIII.cpp
@@ -0,0 +1,24 @@
+// Last updated: 6/3/2026, 10:04:21 PM
+1class Solution {
+2public:
+3    vector<int> singleNumber(vector<int>& nums) {
+4        unordered_map<int, int> obj;
+5        vector<int> res;
+6
+7        for (int charNum : nums) {
+8            if (obj[charNum] == 0) {
+9                obj[charNum] = 1;
+10            } else {
+11                obj[charNum]++;
+12            }
+13        }
+14
+15        for (auto it : obj) {
+16            if (it.second == 1) {
+17                res.push_back(it.first);
+18            }
+19        }
+20
+21        return res;
+22    }
+23};
