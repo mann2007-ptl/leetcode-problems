bbc711bcf8b5809f80a9e1f954ff64100db8b7b2
MANN PATEL <mann.patel.cg@gmail.com>
2026-06-03 11:55:17 +0530

    Create 1748-SumofUniqueElements

---
+++ 1748-SumofUniqueElements/1748-SumofUniqueElements.cpp
@@ -0,0 +1,24 @@
+// Last updated: 6/3/2026, 11:55:15 AM
+1class Solution {
+2public:
+3    int sumOfUnique(vector<int>& nums) {
+4        unordered_map<int, int> obj;
+5        int sum = 0;
+6
+7        for (int num : nums) {
+8            if (obj.find(num) == obj.end()) {
+9                obj[num] = 1;
+10            } else {
+11                obj[num]++;
+12            }
+13        }
+14
+15        for (auto it : obj) {
+16            if (it.second == 1) {
+17                sum += it.first;
+18            }
+19        }
+20
+21        return sum;
+22    }
+23};
