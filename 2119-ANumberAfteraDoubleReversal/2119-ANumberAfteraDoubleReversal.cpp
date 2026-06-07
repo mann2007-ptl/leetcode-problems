2d4bf1fd9d31bc69418df833a962bc8d1e497906
MANN PATEL <mann.patel.cg@gmail.com>
2026-06-07 15:45:39 +0530

    File updated at 6/7/2026, 3:45:36 PM

--- 2119-ANumberAfteraDoubleReversal/2119-ANumberAfteraDoubleReversal.cpp
+++ 2119-ANumberAfteraDoubleReversal/2119-ANumberAfteraDoubleReversal.cpp
@@ -1,7 +1,33 @@
-// Last updated: 6/7/2026, 3:41:32 PM
+// Last updated: 6/7/2026, 3:45:36 PM
 1class Solution {
 2public:
-3    bool isSameAfterReversals(int num) {
-4        return num==0 || num%10 !=0;
-5    }
-6};
+3    string addBinary(string a, string b) {
+4        string result = "";
+5        int i = a.length() - 1;
+6        int j = b.length() - 1;
+7
+8        int carry = 0;
+9
+10        while(i>=0 || j>=0 || carry >0) {
+11            int sum = carry;
+12
+13            if(i>=0) {
+14                sum += a[i] - '0';
+15                i--;
+16            }
+17
+18            if(j>=0) {
+19                sum += b[j] - '0';
+20                j--;
+21            }
+22
+23            
+24            result += to_string(sum%2);
+25
+26            carry = sum/2;
+27        }
+28
+29        reverse(result.begin(), result.end());
+30        return result;
+31    }
+32};
