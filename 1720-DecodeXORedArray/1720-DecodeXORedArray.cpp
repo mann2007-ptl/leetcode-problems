5d997d974a6b423237a410b13ef90d5eee0bd2a3
MANN PATEL <mann.patel.cg@gmail.com>
2026-06-07 21:32:32 +0530

    Create 1720-DecodeXORedArray

---
+++ 1720-DecodeXORedArray/1720-DecodeXORedArray.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/7/2026, 9:32:29 PM
+1class Solution {
+2public:
+3    vector<int> decode(vector<int>& encoded, int first) {
+4        vector<int> ans;
+5        ans.push_back(first);
+6
+7        for(int x : encoded){
+8            ans.push_back(ans.back()^x);
+9        }
+10        return ans;
+11    }
+12};
