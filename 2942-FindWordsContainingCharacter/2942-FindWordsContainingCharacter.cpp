37b37ccaea4f02495182587bf2df972208242687
MANN PATEL <mann.patel.cg@gmail.com>
2026-06-13 11:32:55 +0530

    Create 2942-FindWordsContainingCharacter

---
+++ 2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/13/2026, 11:32:50 AM
+1class Solution {
+2public:
+3    vector<int> findWordsContaining(vector<string>& words, char x) {
+4        vector<int> res;
+5
+6        for(int i=0;i<words.size();i++){
+7            for(char ch : words[i]){
+8                if(ch==x){
+9                    res.push_back(i);
+10                    break;
+11                }
+12            }
+13        }
+14        return res;
+15    }
+16};
