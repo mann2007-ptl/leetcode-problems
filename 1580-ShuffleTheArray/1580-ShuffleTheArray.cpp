ec6e19abe7d7dc7310bef59831228ee806260d71
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:26:30 +0530

    Create 1580-ShuffleTheArray

---
+++ 1580-ShuffleTheArray/1580-ShuffleTheArray.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/17/2026, 4:26:17 PM
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans;
+
+        for(int i=0;i<n;i++){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
+        }
+        return ans;
+    }
+};
