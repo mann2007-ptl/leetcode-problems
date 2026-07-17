1d0903c0b192ee61c6bdf773043fbe8b42abe51d
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:24:46 +0530

    Create 2608-CountTheDigitsThatDivideANumber

---
+++ 2608-CountTheDigitsThatDivideANumber/2608-CountTheDigitsThatDivideANumber.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/17/2026, 4:24:33 PM
+class Solution {
+public:
+    int countDigits(int num) {
+        int original = num;
+
+        int count = 0;
+
+        while(num > 0){
+
+            int digit = num % 10;
+
+            if(original % digit == 0){
+                count++;
+            }
+
+            num = num / 10;
+        }
+
+        return count;
+       
+    }
+};
