fb21a658f3d77ab8959bf23af2c9c55e21f17155
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:24:41 +0530

    Create 2752-SumMultiples

---
+++ 2752-SumMultiples/2752-SumMultiples.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/17/2026, 4:24:28 PM
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum = 0;
+
+        for(int i=1;i<=n;i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+};
