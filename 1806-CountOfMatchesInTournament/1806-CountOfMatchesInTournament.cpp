d3bafcf5d2a8d953a27e975deff7c209a4eca515
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:26:15 +0530

    Create 1806-CountOfMatchesInTournament

---
+++ 1806-CountOfMatchesInTournament/1806-CountOfMatchesInTournament.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/17/2026, 4:26:02 PM
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int match = 0;
+        while(n>1){
+            if(n%2==0){
+                match += n/2;
+                n =n/2;
+            }
+            else{
+                match += (n-1)/2;
+                n =(n-1)/2+1;
+            }
+        }
+        return match;
+    }
+};
