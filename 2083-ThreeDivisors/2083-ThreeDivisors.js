0e65551647f7a439d862496ae438e252600d6d92
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:26:00 +0530

    Create 2083-ThreeDivisors

---
+++ 2083-ThreeDivisors/2083-ThreeDivisors.js
@@ -0,0 +1,18 @@
+// Last updated: 7/17/2026, 4:25:47 PM
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isThree = function(n) {
+    let count = 0;
+    for(let i=1;i<=n;i++){
+        if(n%i==0){
+            count++;
+        }
+    }
+
+    if(count==3){
+        return true;
+    }
+    return false;
+};
