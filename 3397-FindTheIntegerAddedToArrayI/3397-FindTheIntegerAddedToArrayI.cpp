a6cab72f1025539a7265e74b25cd913b97bedfb9
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:24:11 +0530

    Create 3397-FindTheIntegerAddedToArrayI

---
+++ 3397-FindTheIntegerAddedToArrayI/3397-FindTheIntegerAddedToArrayI.cpp
@@ -0,0 +1,9 @@
+// Last updated: 7/17/2026, 4:23:58 PM
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+          sort(nums1.begin(),nums1.end());
+          sort(nums2.begin(),nums2.end());
+          return nums2[0] - nums1[0];
+    }
+};
