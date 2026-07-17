59f10061406d21a2caeeffeb801e7d4b85f29479
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:27:04 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/17/2026, 4:26:51 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i = 0;
+        int j = nums.size() - 1;
+        while(i <= j){
+            int mid = i + (j - i)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                i = mid + 1;
+            }
+            else{
+                j = mid -1;
+            }
+        }
+        return -1;
+    }
+};
