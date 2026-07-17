6bbb2aab57c58393644ec722b1be8a9581eb8b2c
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:24:39 +0530

    Create 2789-CounterIi

---
+++ 2789-CounterIi/2789-CounterIi.js
@@ -0,0 +1,32 @@
+// Last updated: 7/17/2026, 4:24:26 PM
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let value = init;
+
+    return {
+        increment:function(){
+            value++;
+            return value;
+        },
+        
+        decrement:function(){
+            value--;
+            return value;
+        },
+
+        reset:function(){
+            value=init;
+            return value;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
