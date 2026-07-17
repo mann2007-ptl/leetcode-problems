4fe5931957f5327542247d70f7a9ccef4f94f504
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:24:13 +0530

    Create 3397-FindTheIntegerAddedToArrayI

---
+++ 3397-FindTheIntegerAddedToArrayI/3397-FindTheIntegerAddedToArrayI.js
@@ -0,0 +1,12 @@
+// Last updated: 7/17/2026, 4:24:00 PM
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var addedInteger = function(nums1, nums2) {
+    nums1 = nums1.sort((a,b)=>a-b);
+    nums2 = nums2.sort((a,b)=>a-b);
+
+    return nums2[0] - nums1[0]
+};
