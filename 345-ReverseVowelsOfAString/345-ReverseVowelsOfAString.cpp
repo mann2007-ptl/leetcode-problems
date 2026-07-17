6b6ff768a0841a65f233c35f627f1f54559ae590
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-17 16:27:38 +0530

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/17/2026, 4:27:25 PM
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j){
+            if(!(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o'||s[i]=='u'||s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O'||s[i]=='U')){
+                i++;
+            }
+            else if(!(s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o'||s[j]=='u'||s[j]=='A' || s[j]=='E' || s[j]=='I' || s[j]=='O'||s[j]=='U')){
+                j--;
+            }
+            else{
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
