a5985b5465e45581fcd6db53e58426930658b585
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-27 15:14:04 +0530

    Create 1227-AirplaneSeatAssignmentProbability

---
+++ 1227-AirplaneSeatAssignmentProbability/version/cpp/1227-AirplaneSeatAssignmentProbability_20260727_151348.cpp
@@ -0,0 +1,10 @@
+// Last updated: 7/27/2026, 3:13:48 PM
+1class Solution {
+2public:
+3    double nthPersonGetsNthSeat(int n) {
+4        if(n==1){
+5            return 1;
+6        }
+7        return 0.5;
+8    }
+9};
