b41246c7805fcb102828a4211b5fa981a7a654bc
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-27 14:43:39 +0530

    Create

---
+++ problem-1785143603355/version/cpp/problem-1785143603355_20260727_144323.cpp
@@ -0,0 +1,10 @@
+// Last updated: 7/27/2026, 2:43:23 PM
+1class Solution {
+2public:
+3    bool divisorGame(int n) {
+4       if(n%2==0){
+5        return true;
+6       }
+7       return false;
+8    }
+9};
