fd1f7a30a2c86a3c44db279b9561f9689ca25048
MANN PATEL <mann.patel.cg@gmail.com>
2026-07-27 14:44:47 +0530

    Create 292-NimGame

---
+++ 292-NimGame/version/cpp/292-NimGame_20260727_144430.cpp
@@ -0,0 +1,10 @@
+// Last updated: 7/27/2026, 2:44:30 PM
+1class Solution {
+2public:
+3    bool canWinNim(int n) {
+4       if(n%4==0){
+5        return false;
+6       } 
+7       return true;
+8    }
+9};
